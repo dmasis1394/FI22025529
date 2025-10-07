fa84cb34a462f0e671dea7d96d3fb9a3ccd6fe49
dmasis1394 <123431997+dmasis1394@users.noreply.github.com>
2025-10-07 17:44:50 -0600

    PP2

---
+++ PP2/BinaryOperations.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using BinaryOperations.Web.Models;
+using BinaryOperations.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BinaryOperations.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBinaryOperationService _binaryService;
+
+        public HomeController(IBinaryOperationService binaryService)
+        {
+            _binaryService = binaryService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new BinaryOperationModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(BinaryOperationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Results = _binaryService.CalculateOperations(model.A, model.B);
+                    model.HasResults = true;
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", $"Error al calcular las operaciones: {ex.Message}");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ PP2/BinaryOperations.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BinaryOperations.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
